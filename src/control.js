409d6a0be0c6eff8eb1dd517188244bccc067603
Cory Hughart <cr0ybot@gmail.com>
2023-10-16 21:23:47 -0400

    emoji picker control required useRef for reasons, switch to using css custom property instead of data attribute

---
+++ src/control.js
@@ -0,0 +1,140 @@
+/**
+ * Control.
+ */
+
+import EmojiPicker from 'emoji-picker-react';
+
+import {
+	BaseControl,
+	Button,
+	Dropdown,
+	Flex,
+	FlexItem,
+	TextControl,
+	ToolbarButton,
+} from '@wordpress/components';
+import { useRefEffect } from '@wordpress/compose';
+import { useRef, useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+export function EmojiPickerControl({
+	value = '',
+	onChange,
+	onCancel,
+	...props
+}) {
+	const emojiRef = useRef(value);
+	const [emojiState, setEmojiState] = useState(emojiRef.current);
+
+	console.log({
+		emoji: emojiRef.current,
+		length: emojiRef.current.length,
+	});
+
+	/**
+	 * Update emojiState when emojiRef changes.
+	 */
+	useRefEffect(
+		(ref) => {
+			setEmojiState(ref.current);
+		},
+		[emojiRef]
+	);
+
+	/**
+	 * Add the selected emoji to the end of the input value.
+	 *
+	 * Note: Don't bother with useCallback here. It doesn't work because the
+	 * emoji picker is heavily memoized internally and holds on to the first
+	 * reference to onEmojiClick so the entire picker doesn't rerender.
+	 * Instead, were using a ref to store the current emoji value.
+	 *
+	 * @see https://github.com/ealush/emoji-picker-react/issues/365
+	 */
+	const handleEmojiClick = ({ emoji: selectedEmoji }) => {
+		const emoji = emojiRef.current;
+		console.log(emoji, '+', selectedEmoji);
+		console.log(emoji + selectedEmoji);
+		emojiRef.current = emoji + selectedEmoji;
+		setEmojiState(emojiRef.current);
+	};
+
+	const handleTextChange = (newEmoji) => {
+		emojiRef.current = newEmoji;
+		setEmojiState(newEmoji);
+	};
+
+	return (
+		<BaseControl
+			className="any-emoji-separator-picker"
+			label={__('Select emoji to add', 'any-emoji-separator')}
+			{...props}
+		>
+			<Flex gap={1}>
+				<FlexItem>
+					<TextControl
+						className="any-emoji-separator-picker__input"
+						value={emojiState}
+						onChange={handleTextChange}
+						placeholder={__(
+							'Type any character or emoji',
+							'any-emoji-separator'
+						)}
+					/>
+				</FlexItem>
+				<FlexItem>
+					<Button
+						size="small"
+						text={__('Done', 'any-emoji-separator')}
+						onClick={() => onChange?.(emojiRef.current)}
+					/>
+				</FlexItem>
+				<FlexItem>
+					<Button
+						size="small"
+						text={__('Cancel', 'any-emoji-separator')}
+						onClick={() => onCancel()}
+					/>
+				</FlexItem>
+			</Flex>
+			<EmojiPicker
+				onEmojiClick={handleEmojiClick}
+				emojiStyle="native"
+				previewConfig={{ defaultCaption: 'Add anything!' }}
+			/>
+		</BaseControl>
+	);
+}
+
+export function EmojiPickerDropdown({ value, onChange, ...props }) {
+	return (
+		<Dropdown
+			className="any-emoji-separator-dropdown"
+			contentClassName="any-emoji-separator-dropdown__content"
+			position="bottom right"
+			renderToggle={({ isOpen, onToggle }) => (
+				<ToolbarButton
+					className="any-emoji-separator-dropdown__toggle"
+					icon="smiley"
+					label={__('Select an emoji to add', 'any-emoji-separator')}
+					onClick={onToggle}
+					isPressed={isOpen}
+					aria-expanded={isOpen}
+				/>
+			)}
+			renderContent={({ onToggle }) => (
+				<EmojiPickerControl
+					value={value}
+					onChange={(newValue) => {
+						onChange?.(newValue);
+						onToggle();
+					}}
+					onCancel={() => onToggle()}
+				/>
+			)}
+			{...props}
+		/>
+	);
+}
+
+export default EmojiPickerDropdown;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,125 @@
  * Script entrypoint.
  */
 
-import "./style.scss";
-import "./editor.scss";
+// Import styles for webpack to process.
+import './style.scss';
+import './editor.scss';
 
-console.log("hello world");
+// WordPress dependencies.
+import { BlockControls } from '@wordpress/block-editor';
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { addFilter } from '@wordpress/hooks';
+
+// Internal dependencies.
+import EmojiPickerDropdown from './control';
+
+/**
+ * Add a custom attribute to the separator block.
+ */
+const addAnyEmojiAttribute = (settings, name) => {
+	if (name !== 'core/separator') {
+		return settings;
+	}
+
+	return {
+		...settings,
+		attributes: {
+			...settings.attributes,
+			anyEmoji: {
+				type: 'string',
+				default: '',
+			},
+		},
+	};
+};
+addFilter(
+	'blocks.registerBlockType',
+	'any-emoji-separator/add-any-emoji-attribute',
+	addAnyEmojiAttribute
+);
+
+/**
+ * Adds the emoji picker to the separator block.
+ */
+const withEmojiPickerControl = createHigherOrderComponent(
+	(BlockEdit) => (props) => {
+		if (props.name !== 'core/separator') {
+			return <BlockEdit {...props} />;
+		}
+
+		const { attributes, setAttributes } = props;
+		const { anyEmoji } = attributes;
+
+		console.log({ anyEmoji });
+
+		return (
+			<>
+				<BlockControls group="other">
+					<EmojiPickerDropdown
+						value={anyEmoji}
+						onChange={(emoji) => setAttributes({ anyEmoji: emoji })}
+					/>
+				</BlockControls>
+				<BlockEdit {...props} />
+			</>
+		);
+	},
+	'withEmojiPickerControl'
+);
+addFilter(
+	'editor.BlockEdit',
+	'any-emoji-separator/with-emoji-picker-control',
+	withEmojiPickerControl
+);
+
+/**
+ * Adds the emoji to the separator block on save via style attribute.
+ */
+const addAnyEmojiOnSave = (props, blockType, attributes) => {
+	if (blockType.name !== 'core/separator') {
+		return props;
+	}
+
+	const { anyEmoji } = attributes;
+	const hasEmoji = anyEmoji.length > 0;
+
+	return {
+		...props,
+		...(hasEmoji ? { style: `--any-emoji: '${anyEmoji}'` } : {}),
+	};
+};
+addFilter(
+	'blocks.getSaveContent.extraProps',
+	'any-emoji-separator/add-any-emoji-on-save',
+	addAnyEmojiOnSave
+);
+
+/**
+ * Adds the emoji to the separator block in the editor via BlockListBlock filter.
+ */
+const withAnyEmojiInEditor = createHigherOrderComponent(
+	(BlockListBlock) => (props) => {
+		if (props.name !== 'core/separator') {
+			return <BlockListBlock {...props} />;
+		}
+
+		const { attributes } = props;
+		const { anyEmoji } = attributes;
+		const hasEmoji = anyEmoji.length > 0;
+
+		return (
+			<BlockListBlock
+				{...props}
+				{...(hasEmoji
+					? { wrapperProps: { style: { '--any-emoji': `'${anyEmoji}'` } } }
+					: {})}
+			/>
+		);
+	},
+	'withAnyEmojiInEditor'
+);
+addFilter(
+	'editor.BlockListBlock',
+	'any-emoji-separator/with-any-emoji-in-editor',
+	withAnyEmojiInEditor
+);
