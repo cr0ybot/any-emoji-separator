9eae095d5df3488cf6da3bc44102b5595474f6c1
Cory Hughart <cr0ybot@gmail.com>
2023-10-18 20:23:52 -0400

    go back to useCallback for emoji picker after upstream fix https://github.com/ealush/emoji-picker-react/issues/365

--- src/control.js
+++ src/control.js
@@ -14,8 +14,7 @@ import {
 	TextControl,
 	ToolbarButton,
 } from '@wordpress/components';
-import { useRefEffect } from '@wordpress/compose';
-import { useRef, useState } from '@wordpress/element';
+import { useCallback, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 export function EmojiPickerControl({
@@ -24,45 +23,20 @@ export function EmojiPickerControl({
 	onCancel,
 	...props
 }) {
-	const emojiRef = useRef(value);
-	const [emojiState, setEmojiState] = useState(emojiRef.current);
-
-	console.log({
-		emoji: emojiRef.current,
-		length: emojiRef.current.length,
-	});
-
-	/**
-	 * Update emojiState when emojiRef changes.
-	 */
-	useRefEffect(
-		(ref) => {
-			setEmojiState(ref.current);
-		},
-		[emojiRef]
-	);
+	const [emoji, setEmoji] = useState(value);
 
 	/**
 	 * Add the selected emoji to the end of the input value.
-	 *
-	 * Note: Don't bother with useCallback here. It doesn't work because the
-	 * emoji picker is heavily memoized internally and holds on to the first
-	 * reference to onEmojiClick so the entire picker doesn't rerender.
-	 * Instead, were using a ref to store the current emoji value.
-	 *
-	 * @see https://github.com/ealush/emoji-picker-react/issues/365
 	 */
-	const handleEmojiClick = ({ emoji: selectedEmoji }) => {
-		const emoji = emojiRef.current;
-		console.log(emoji, '+', selectedEmoji);
-		console.log(emoji + selectedEmoji);
-		emojiRef.current = emoji + selectedEmoji;
-		setEmojiState(emojiRef.current);
-	};
+	const handleEmojiClick = useCallback(
+		({ emoji: selectedEmoji }) => {
+			setEmoji(emoji + selectedEmoji);
+		},
+		[emoji]
+	);
 
 	const handleTextChange = (newEmoji) => {
-		emojiRef.current = newEmoji;
-		setEmojiState(newEmoji);
+		setEmoji(newEmoji);
 	};
 
 	return (
@@ -75,7 +49,7 @@ export function EmojiPickerControl({
 				<FlexBlock>
 					<TextControl
 						className="any-emoji-separator-picker__input"
-						value={emojiState}
+						value={emoji}
 						onChange={handleTextChange}
 						placeholder={__(
 							'Type any character or emoji',
@@ -88,7 +62,7 @@ export function EmojiPickerControl({
 						size="small"
 						variant="primary"
 						text={__('Apply', 'any-emoji-separator')}
-						onClick={() => onChange?.(emojiRef.current)}
+						onClick={() => onChange?.(emoji)}
 					/>
 				</FlexItem>
 				<FlexItem>
